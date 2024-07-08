55406571350efcb11f990eb785d22057b16e7d62
Nischal <nischalshrestha800@gmail.com>
2024-07-08 11:03:22 +0545

    Contains login authentication

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('books.list');
+    }
+
+    public function store()
+    {
+    }
+
+    public function edit()
+    {
+    }
+    public function update()
+    {
+    }
+    public function destroy()
+    {
+    }
+}
